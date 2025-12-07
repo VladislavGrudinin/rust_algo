db4bc0a7d4f4331451df6f8e58b78c5d096d116e
Vladislav Grudinin <vlad.grudinin@gmail.com>
2025-12-07 11:38:50 +0700

    rename

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 #![allow(dead_code, unused_imports)]
 #![allow(non_snake_case)]
-pub mod algo;
-pub mod dsu;
-pub mod fenwick;
-pub mod geometry;
-pub mod io;
-pub mod math;
-pub mod mod_int;
-pub mod segment_tree;
+
+mod cp;
+pub use crate::cp::*;
